0ea95d0decf2d49c4e392e0a7fbe183594f8a3d2
jungyw <jungyw@emforce.co.kr>
2020-06-29 14:07:09 +0900

    ekams

--- src/lib/feature/InflowCall.js
+++ src/lib/feature/InflowCall.js
@@ -1,9 +1,12 @@
+/* eslint-disable no-console */
 import jsCookie from 'js-cookie';
 import queryString from 'query-string';
 import state from '@@state';
 import callTrackSever from '../module/CallTrackServer';
 
 const { configs } = state;
+
+const NONE = 'none';
 
 // const ekamsConfig = {
 //   key: 'EKAMS',
@@ -24,85 +27,109 @@ const inflowCall = (advertiserId /* , args */) => {
   const query = queryString.parse(window.location.search);
   const queryEkams = query.EKAMS;
   const config = configs[advertiserId];
-  const sendQuery = {}; // click 서버에 보내는 쿼리 목록
+  let sendQuery = {}; // click 서버에 보내는 쿼리 목록
   const sendQueryKeyToCookieKey = {}; // 쿠키 어시스트 조정용
 
-  if (Number(queryEkams?.split('.')[1]) === advertiserId) {
+  // ekams의 광고주 id가 inflow의 광고주 id와 다를 경우 호출 안함.
+  if (Number(queryEkams?.split('.')[1]) !== Number(advertiserId)) {
+    console.log('??');
     return;
   }
 
   let isCallCondition = !!queryEkams || config.requireInflowQuery === true;
   const { trackQueryKeyList } = config;
   trackQueryKeyList.forEach((elmList) => {
-    const isSatisfied = elmList.every((elm) => {
+    const currentValidateQueryKey = {};
+    let isSatisfied = true;
+    elmList.forEach((elm) => {
       const { key, exactlyMatch, partialMatch } = elm;
       let { sendQueryKey, cookieKey } = elm;
       sendQueryKey = sendQueryKey || key;
       cookieKey = cookieKey || key;
       sendQueryKeyToCookieKey[sendQueryKey] = cookieKey;
-      const value = query[key];
-      sendQuery[sendQueryKey] = value;
+      const value = query[key] || NONE;
+      currentValidateQueryKey[sendQueryKey] = value;
+
+      // 하나라도 만족하지 않은 경우가 있을 경우, 다른 키 체크 필요 X
+      if (isSatisfied === false) {
+        return;
+      }
       if (exactlyMatch) {
-        return exactlyMatch.includes(value);
+        isSatisfied = exactlyMatch.includes(value);
+      } else if (partialMatch) {
+        isSatisfied = partialMatch.some((regExp) =>
+          new RegExp(regExp).test(value)
+        );
+      } else {
+        isSatisfied = value !== NONE;
       }
-      if (partialMatch) {
-        return partialMatch.some((regExp) => new RegExp(regExp).test(value));
-      }
-      return value;
     });
     if (isSatisfied) {
       isCallCondition = true;
+    } else {
+      // 하나라도 만족하지 않는 경우가 있다면, 모두 none 처리
+      Object.keys(currentValidateQueryKey).forEach((key) => {
+        currentValidateQueryKey[key] = NONE;
+      });
     }
+    sendQuery = {
+      ...sendQuery,
+      ...currentValidateQueryKey,
+    };
   });
-
   if (!isCallCondition) {
     return;
   }
 
   const currentTime = new Date().getTime();
+  const cekams = `${queryEkams || NONE}_${currentTime}`;
+  sendQuery.cekams = cekams;
+  sendQueryKeyToCookieKey.cekams = 'ekams';
   const ekamsInCookie = getCookie(advertiserId, 'ekams');
-
-  Object.keys(sendQuery).forEach((key) => {
-    if (!sendQuery[key]) {
-      sendQuery[key] = 'none';
-    }
-  });
-
-  const cekams = `${queryEkams || 'none'}_${currentTime}`;
-  if (!ekamsInCookie) {
-    console.warn('33');
-  } else {
+  let ekamsLength = 0;
+  let latestSameEkams = false;
+  if (ekamsInCookie) {
     const ekamsList = ekamsInCookie.split('|');
-    const ekamsLength = ekamsList.length;
+    ekamsLength = ekamsList.length;
     const latestIndex = ekamsLength - 1;
-    const latestEkams = ekamsList[latestIndex];
+    const latestEkams = ekamsList[latestIndex].split('_');
     const [ltEkams] = latestEkams;
-    const latestSameEkams = ltEkams === queryEkams;
-    Object.entries(sendQueryKeyToCookieKey).forEach(
-      ([sendQueryKey, cookieKey]) => {
-        const cookies = getCookie(advertiserId, cookieKey) || [];
-        const [first = 'none', assist = 'none', latest = 'none'] = cookies;
-        const changeLatestCookie = sendQuery[sendQueryKey];
-        const changeCookies = [];
-        if (ekamsLength === 3) {
-          changeCookies.push(first);
-          if (latestSameEkams) {
-            changeCookies.push(assist);
-          } else {
-            changeCookies.push(latest);
-          }
-        } else if (ekamsLength === 2) {
-          changeCookies.push(first);
-          if (!latestSameEkams) {
-            changeCookies.push(assist);
-          }
-        } else if (!latestSameEkams) {
-          changeCookies.push(first);
-        }
-        changeCookies.push(changeLatestCookie);
-      }
-    );
+    console.log('lt', ltEkams);
+    console.log('qe', queryEkams);
+    latestSameEkams = ltEkams === queryEkams;
+  } else {
+    setCookie(advertiserId, 'ekams', cekams);
   }
+  console.log(latestSameEkams);
+
+  Object.entries(sendQueryKeyToCookieKey).forEach(
+    ([sendQueryKey, cookieKey]) => {
+      const cookies = getCookie(advertiserId, cookieKey);
+      const [first = NONE, assist = NONE, latest = NONE] = cookies
+        ? cookies.split('|')
+        : [];
+      const changeLatestCookie = sendQuery[sendQueryKey];
+      const changeCookies = [];
+      if (ekamsLength === 3) {
+        changeCookies.push(first);
+        if (latestSameEkams) {
+          changeCookies.push(assist);
+        } else {
+          changeCookies.push(latest);
+        }
+      } else if (ekamsLength === 2) {
+        changeCookies.push(first);
+        if (!latestSameEkams) {
+          changeCookies.push(assist);
+        }
+      } else if (!latestSameEkams && ekamsLength === 1) {
+        changeCookies.push(first);
+      }
+      changeCookies.push(changeLatestCookie);
+      setCookie(advertiserId, cookieKey, changeCookies.join('|'));
+    }
+  );
+  console.log(sendQuery);
   callTrackSever('click', queryString.stringify(sendQuery));
 };
 
