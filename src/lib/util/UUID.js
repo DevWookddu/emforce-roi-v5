478a31f8efa41dcbb301965859cc8d9a8d4ba713
jungyw <jungyw@emforce.co.kr>
2020-07-31 12:12:23 +0900

    v.1.0.2

--- src/lib/util/UUID.js
+++ src/lib/util/UUID.js
@@ -1,11 +1,15 @@
 /* eslint-disable no-bitwise */
 const generateUUID = () => {
   let time = new Date().getTime();
-  return 'exxxxxxx-mxxx-4xxx-sxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (ch) => {
-    const r = (time + Math.random() * 16) % 16 | 0;
-    time = Math.floor(time / 16);
-    return (ch === 'x' ? r : (r & 0x3) | 0x8).toString(16);
-  });
+  try {
+    return 'exxxxxxx-mxxx-4xxx-sxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (ch) => {
+      const r = (time + Math.random() * 16) % 16 | 0;
+      time = Math.floor(time / 16);
+      return (ch === 'x' ? r : (r & 0x3) | 0x8).toString(16);
+    });
+  } catch {
+    return `uuiderror_${time}`;
+  }
 };
 
 export default generateUUID;
